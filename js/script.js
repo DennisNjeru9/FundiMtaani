2060aba37527fe418e8f02e287285aa1e157250f
Ondigo Wendy <wendy.ondigo@student.moringaschool.com>
2021-06-30 11:31:20 +0300

    Change the code to match the home branch.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
 $('#multi').mdbRange({
-  single: {
-    active: true,
-    multi: {
-      active: true,
-      rangeLength: 1
-    },
-  }
+    single: {
+        active: true,
+        multi: {
+            active: true,
+            rangeLength: 1
+        },
+    }
 });
 
 
 // show modal
 $('#myModal').modal('show');
 // www.jquery2dotnet.com
+
+$(document).ready(function() {
+    function recive() {
+        document.getElementById('recive').addEventListener("click", function() {
+            alert('Thank you ' + name + ' we have recieved your application we will contact you via your email');
+        });
+
+
+
+    }
+
+
+
+});
